daacca5aecb4c77f71456332594014e10dd70043
Soochan Lee <soochan.lee@vision.snu.ac.kr>
2018-03-04 15:43:28 +0900

    pyenv setup script

---
+++ setup_pyenv.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshenv
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshenv
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshenv
+
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshenv
+
