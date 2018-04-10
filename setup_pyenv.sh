2cd09ced39f65e499557aad880b2ec3ccb284e06
Soochan Lee <soochan.lee@vision.snu.ac.kr>
2018-04-10 22:02:14 +0900

    fix a bug pyenv setup script

--- setup_pyenv.sh
+++ setup_pyenv.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshenv
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshenv
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshenv
+echo 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshenv
 
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshenv
